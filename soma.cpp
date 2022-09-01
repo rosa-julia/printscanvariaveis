f5b79a2309f1cce604045243e58331f440a2952d
rosa-julia <111510794+rosa-julia@users.noreply.github.com>
2022-09-01 13:54:06 -0300

    Add files via upload

---
+++ soma.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	
+	int numero1, numero2, soma;
+	char sexo, nome[4];
+	float nota;
+	double media;
+	bool presenca;
+	
+	printf("Digite o primeiro numero:");
+	scanf("%d",&numero1);
+	printf("Digite o segundo numero:");
+	scanf("%d",&numero2);
+	printf("Digite o sexo:");
+	scanf("%c",&sexo);
+	printf("Digite a nota:");
+	scanf("%f",&nota);
+	printf("Digite a media:");
+	scanf("%lf",&media);
+	printf("Digite a presenca:");
+	scanf("%b",&presenca);
+	
+}
+
+
